726e5c5f6faccee13f3eca8178f2fa7efa3fb08b
Brian Misachi <misachibrian@gmail.com>
2017-01-29 01:13:53 +0300

    Not working

--- Trial_app/views.py
+++ Trial_app/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-from Trial_app.models import Registration
-from Trial_app.forms import LoginForm, RegistrationForm, ProfileForm
+from Trial_app.forms import RegistrationForm, ProfileForm
 
 
 def index(request):
@@ -11,13 +10,30 @@ def index(request):
 
 
 def home(request):
-    return render(request,'Trial_app/home.html', {})
+    return render(request, 'Trial_app/home.html', {})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_data = user_form.save(commit=False)
+            user_data.set_password(user_data.password)
+            user_data.save(commit=True)
+            return HttpResponseRedirect('/trial/feeds')
+        else:
+            return render(request, 'Trial_app/register.html', {'form': user_form})
+    else:
+        user_form = RegistrationForm()
+
+    return render(request, 'Trial_app/register.html', {'form': user_form})
+
 
 
 @login_required(login_url='login')
 def feeds(request):
     # if request.method == 'POST':
-    return render(request,'Trial_app/feeds.html', {})
+    return render(request, 'Trial_app/feeds.html', {})
 
 
 @login_required(login_url='login')
@@ -27,43 +43,12 @@ def profile(request):
         if profile_info.is_valid():
             profile_info.save(commit=True)
         else:
-            return render(request, 'Trial_app/profile.html', {'form':profile_info})
+            return render(request, 'Trial_app/profile.html', {'form': profile_info})
     else:
         profile_info = ProfileForm()
-    return render(request, 'Trial_app/profile.html', {'form':profile_info})
+    return render(request, 'Trial_app/profile.html', {'form': profile_info})
 
 
 @login_required(login_url='login')
 def message(request):
     return render(request, 'Trial_app/message.html', {})
-
-# def register(request):
-#     if request.method == "POST":
-#         user_details = RegistrationForm(request.POST)
-#         if user_details.is_valid():
-#             form = user_details.save(commit=False)
-#             form.set_password(form.password)
-#             form.save()
-#             return HttpResponseRedirect('/trial/')
-#         else:
-#             return render(request, 'Trial_app/register.html', {'form' : user_details})
-#     else:
-#         user_details = RegistrationForm()
-#
-#     return render(request, 'Trial_app/register.html', {'form' : user_details})
-
-
-def register(request):
-    if request.method == 'POST':
-        user_form = RegistrationForm(request.POST)
-        if user_form.is_valid():
-            user = user_form.save(commit=False)
-            user.set_password(user.password)
-            user.save(commit=True)
-            return HttpResponseRedirect('/trial/')
-        else:
-            return render(request, 'Trial_app/register.html', {'form' : user_form})
-    else:
-        user_form = RegistrationForm()
-
-    return render(request, 'Trial_app/register.html', {'form' : user_form})
