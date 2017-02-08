755283a261c9bb5c07e92837f57455658ef46040
Brian Misachi <misachibrian@gmail.com>
2017-02-09 00:59:10 +0300

    Modified feeds view

--- feeds/views.py
+++ feeds/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from feeds.models import Feed
 
 
 @login_required(login_url='login')
 def feeds(request):
-    # if request.method == 'POST':
-    return render(request, 'feeds/feeds.html', {})
+    if request.method == 'POST':
+        feeds_data = request.POST['feeds']
+        Feed.objects.create(feeds=feeds_data)
+    else:
+        return HttpResponse("Only Post Allowed")
+    return render(request, 'feeds/feeds.html', {'form': feeds_data})
