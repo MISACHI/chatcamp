6001d84fce46ac5bb19a90c06650ae1396f0096d
Brian Misachi <misachibrian@gmail.com>
2017-02-24 02:27:16 +0300

    Still trying to fix error

--- Trial_project/settings.py
+++ Trial_project/settings.py
@@ -94,13 +94,14 @@ if config('DB_HOST'):
         }
     }
 else:
-    DATABASES = {
-        'default': {
-            dj_database_url.config(
-                default=config('DATABASE_URL'),
-            )
-        }
-    }
+    DATABASES['default'] = dj_database_url.config()
+    # DATABASES = {
+    #     'default': {
+    #         dj_database_url.config(
+    #             default=config('DATABASE_URL'),
+    #         )
+    #     }
+    # }
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Password validation
