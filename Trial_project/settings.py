be5055fb8ee8f443d01f623000d0a528710ba96e
Brian Misachi <misachibrian@gmail.com>
2017-02-23 18:39:41 +0300

    Added the port number - Bug still exists

--- Trial_project/settings.py
+++ Trial_project/settings.py
@@ -90,7 +90,7 @@ if config('DB_HOST'):
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
             'HOST': config('DB_HOST'),
-            'PORT': '',
+            'PORT': config('PORT'),
         }
     }
 else:
